43ff94acf6652e680cd2521aca9249a6f14335e2
Noch4nce <m4rgera123@mail.ru>
2022-11-14 16:05:13 +0500

    main layout

--- src/App.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import './styles/global.scss'
-
-const App = () => {
-	return <div>Hey</div>
-}
-
-export default App
---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import MainLayout from './layouts/MainLayout'
+import './styles/global.scss'
+
+const App = () => {
+	return (
+		<div>
+			<MainLayout>
+				Card
+			</MainLayout>
+		</div>
+	)
+}
+
+export default App
